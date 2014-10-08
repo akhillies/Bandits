bcbf7155fd0d30006b49b605a310606c0d3cea16
Akhil Batra <akhilbatra@berkeley.edu>
2014-10-07 23:44:05 -0700

    updating pages

--- js/page-nav.js
+++ js/page-nav.js
@@ -1,36 +1,41 @@
 $('#home').click(function()
 {
     $('#pagediv').html('');
-    $('#pagediv').slideUp();
+    $('#pagediv').slideUp(1000);
 });
 
 $('#pitch').click(function()
 {
     $('#pagediv').load('html/pitch.html');
-    $('#pagediv').slideDown();
+    $('#pagediv').slideDown(1000);
 });
 
 $('#storyboard').click(function()
 {
     $('#pagediv').load('html/storyboard.html');
+    $('#pagediv').slideDown();
 });
 
 $('#animatic').click(function()
 {
     $('#pagediv').load('html/animatic.html');
+    $('#pagediv').slideDown();
 });
 
 $('#conceptart').click(function()
 {
     $('#pagediv').load('html/conceptart.html');
+    $('#pagediv').slideDown();
 });
 
 $('#characterdesign').click(function()
 {
     $('#pagediv').load('html/characterdesign.html');
+    $('#pagediv').slideDown();
 });
 
 $('#model').click(function()
 {
     $('#pagediv').load('html/model.html');
+    $('#pagediv').slideDown();
 });

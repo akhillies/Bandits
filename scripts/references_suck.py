0a526be9930ddcb0455e905efb565c912e9e8406
saurabhmitra <saurabh.mitra@berkeley.edu>
2015-01-16 22:36:22 -0800

    I'm about to check myself before i wreck myself

--- scripts/references_suck.py
+++ scripts/references_suck.py
@@ -1,15 +1,34 @@
-search_string = 'Bandits/maya'
+import fnmatch
+import os
+
+search_strings = ['/Bandits/maya/', '/Bandits//maya/']
 def fix_file(name):
 	f = open(name, 'r')
 	output = ""
+	modified = False;
 	for line in f:
-		ind = line.find(search_string)
-		if ind > 0:
-			left_quot = line[:ind].rfind('"')
-			line = line[:left_quot+1] + line[ind + len(search_string) + 1:]
+		for search_string in search_strings:
+			ind = line.find(search_string)
+			if ind >= 0:
+				print "ORIGINAL:   " + line
+				left_quot = line[:ind].rfind('"')
+				num_slashes = {True: 2, False: 1}[line[ind + len(search_string) + 2] == '/']
+				line = line[:left_quot+1] + line[ind + len(search_string) + num_slashes:]
+				modified = True
+				print 'CHANGED TO: ' + line
 		output += line
 	f.close()
-	f = open(name, 'w')
-	f.write(output)
+	# if modified:
+	# 	f = open(name, 'w')
+	# 	f.write(output)
 
-fix_file('../maya/scenes/TRYTHIS.ma')
+
+maya_files = []
+for root, dirnames, filenames in os.walk('../maya'):
+  for filename in fnmatch.filter(filenames, '*.ma'):
+      maya_files.append(os.path.join(root, filename))
+
+for i in maya_files:
+	print "FILE: " + i
+	if i.find('keys') < 0:
+		fix_file(i)

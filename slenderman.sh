5244a94b1879d0f583bd98f3c9b89829308c8acd
Akhil Batra <akhilbatra@berkeley.edu>
2015-04-05 16:26:30 -0700

    updated scripts

--- slenderman.sh
+++ slenderman.sh
@@ -1,2 +1,2 @@
-python /home/cc/cs198/sp15/class/cs198-ed/CNM190/Bandits/git.py
+
 /Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render -r rman -ris -rd /home/tmp/cs198-ed/Bandits/frames -cam '$2' -res 960 540 -s $3 -e $4 -im 'frame' -of 'OpenEXR' -fnc 'name_#.ext' -pad 3 -spool 'immediate rib, remote render' $1

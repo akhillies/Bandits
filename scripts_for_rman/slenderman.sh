1168f83b02c6c23fc28c68b993dcd6a68060d2f8
Akhil Batra <akhilbatra@berkeley.edu>
2015-04-06 00:43:02 -0700

    tesT

--- scripts_for_rman/slenderman.sh
+++ scripts_for_rman/slenderman.sh
@@ -1,4 +1,3 @@
 fname=$(basename $1 .ma);
 
-/Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render -r rman -ris -rd /home/tmp/cs198-ed/Bandits/frames/$fname -cam $2 -res 960 540 -s $3 -e $4 -im '$fname' -of 'OpenEXR' -fnc 'name_#.ext' -pad 3 -spool 'immediate rib, remote render' -proj "/home/cc/cs198/sp15/class/cs198-ed/CNM190/Bandits/maya"
- $1
+/Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render -r rman -ris -rd /home/tmp/cs198-ed/Bandits/frames -cam $2 -res 960 540 -s $3 -e $4 -im $fname -of 'OpenEXR' -fnc 'name_#.ext' -pad 3 -spool 'immediate rib, remote render' -proj "/home/cc/cs198/sp15/class/cs198-ed/CNM190/Bandits/maya" $1

49f79cb82838c64e623f2896eda8193173b2d03e
Akhil Batra <akhilbatra@berkeley.edu>
2015-04-05 17:26:49 -0700

    up

--- scripts_for_rman/sshlogin.sh
+++ scripts_for_rman/sshlogin.sh
@@ -8,7 +8,7 @@ set cam [lindex $argv 4]
 set startframe [lindex $argv 5]
 set endframe [lindex $argv 6]
 
-spawn ssh -oStrictHostKeyChecking=no -oCheckHostIP=no $usr@$host "python /home/cc/cs198/sp15/class/cs198-ed/CNM190/Bandits/git.py; /home/cc/cs198/sp15/class/cs198-ed/CNM190/Bandits/slenderman.sh $file $cam $startframe $endframe"
+spawn ssh -oStrictHostKeyChecking=no -oCheckHostIP=no $usr@$host "python /home/cc/cs198/sp15/class/cs198-ed/CNM190/Bandits/scripts_for_rman/git.py; /home/cc/cs198/sp15/class/cs198-ed/CNM190/Bandits/scripts_for_rman/slenderman.sh $file $cam $startframe $endframe"
 expect "Password:"
 send "$pw\r" 
 interact > /dev/null;

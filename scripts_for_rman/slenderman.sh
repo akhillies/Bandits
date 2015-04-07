09df96cec1715977d08402e8dbe8029377bd9d33
Akhil Batra <akhilbatra@berkeley.edu>
2015-04-07 16:24:44 -0700

    auto push!!!!!!!

--- scripts_for_rman/slenderman.sh
+++ scripts_for_rman/slenderman.sh
@@ -17,17 +17,17 @@ spinner()
 
 fname=$(basename $5 .ma);
 
-echo "\n\n==========================================\n\n"
+printf "\n\n==========================================\n\n"
 git -C $1 fetch --depth=1 &
 spinner $! "Updating github folder:"
 wait
 git -C $1 reset --hard origin/master &
 spinner $! "Running <git reset>:"
 wait
-echo "\n\n==========================================\n\n"
+printf "\n\n==========================================\n\n"
 
-echo "CALLING RENDER:\n\n"
+printf "CALLING RENDER:\n\n"
 
 /Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render -r rman -ris -rd "$4/$fname" -cam $6 -res 960 540 -s $7 -e $8 -im $fname -of 'OpenEXR' -fnc 'name_#.ext' -pad 3 -spool 'immediate rib, remote render' -proj "$1/$2" $1/$2/$3/$5
 
-echo "\n____________________________________\nPlease wait for email with command to run and transfer all your rendered images to your computer"
+printf "\n____________________________________\nPlease wait for email with command to run and transfer all your rendered images to your computer"

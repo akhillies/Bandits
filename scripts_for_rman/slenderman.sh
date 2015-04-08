f855eb04ca6e0494f6e6d01a53998fcd4a419e29
Akhil Batra <akhilbatra@berkeley.edu>
2015-04-07 20:40:07 -0700

    auto push!!!!!!!

--- scripts_for_rman/slenderman.sh
+++ scripts_for_rman/slenderman.sh
@@ -28,4 +28,4 @@ printf "\n=============================================\n\n"
 
 printf "Rendering:\n\n"
 
-/Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render -r rman -ris -rd "$4/$fname" -cam $6 -res 1920 1080 -s $7 -e $8 -im $fname -of 'OpenEXR' -fnc 'name_#.ext' -pad 3 -spool 'immediate rib, remote render' -proj "$1/$2" -postRender 'python("execfile(\"p.py\")")' $1/$2/$3/$5
+/Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render -r rman -ris -rd "$4/$fname" -cam $6 -res 1920 1080 -s $7 -e $8 -im $fname -of 'OpenEXR' -fnc 'name_#.ext' -pad 3 -spool 'immediate rib, remote render' -proj "$1/$2" -postRender 'python("execfile(\"/home/cc/cs198/sp15/class/cs198-ed/p.py\")")' $1/$2/$3/$5

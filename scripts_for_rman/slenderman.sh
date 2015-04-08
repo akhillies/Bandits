f39308fa649605d6fab2956f2a5e85544527905b
Akhil Batra <akhilbatra@berkeley.edu>
2015-04-08 14:29:54 -0700

    auto push!!!!!!!

--- scripts_for_rman/slenderman.sh
+++ scripts_for_rman/slenderman.sh
@@ -28,4 +28,9 @@ printf "\n=============================================\n\n"
 
 printf "Rendering:\n\n"
 
-/Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render -r rman -ris -rd "$4/$fname" -cam $6 -res 1920 1080 -s $7 -e $8 -im $fname -of 'OpenEXR' -fnc 'name_#.ext' -pad 3 -spool 'immediate rib, remote render' -proj "$1/$2" $1/$2/$3/$5
+/Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render -r rman -ris -rd "$4/$fname" -cam $6 -res 1920 1080 -s $7 -e $8 -im $fname -of 'OpenEXR' -fnc 'name_#.ext' -pad 3 -spool 'immediate rib, remote render' -proj "$1/$2" $1/$2/$3/$5 &
+logout
+
+
+#spinner $! "Working on rendering..."
+ls -l | grep -v ^l | wc -l

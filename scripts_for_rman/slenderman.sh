49fe2156a2e268ec6167e98c391123919b0446d6
Akhil Batra <akhilbatra@berkeley.edu>
2015-04-07 21:57:00 -0700

    auto push!!!!!!!

--- scripts_for_rman/slenderman.sh
+++ scripts_for_rman/slenderman.sh
@@ -28,4 +28,4 @@ printf "\n=============================================\n\n"
 
 printf "Rendering:\n\n"
 
-/Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render -r rman -ris -rd "$4/$fname" -cam $6 -res 1920 1080 -s $7 -e $8 -im $fname -of 'OpenEXR' -fnc 'name_#.ext' -pad 3 -spool 'immediate rib, remote render' -proj "$1/$2" -postRender "/usr/sbin/Mail -s 'Job done: %j' %u < %f (%j = job title, %t = task title, %u = user, %f = temporary status file)" $1/$2/$3/$5
+/Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render -r rman -ris -rd "$4/$fname" -cam $6 -res 1920 1080 -s $7 -e $8 -im $fname -of 'OpenEXR' -fnc 'name_#.ext' -pad 3 -spool 'immediate rib, remote render' -proj "$1/$2" -postRender '/usr/sbin/Mail -s "Job done: %j" %u < %f (%j = job title, %t = task title, %u = user, %f = temporary status file)' $1/$2/$3/$5

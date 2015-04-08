5460b82aae0a95262986279db2e66e44e66fe5aa
Akhil Batra <akhilbatra@berkeley.edu>
2015-04-08 14:41:11 -0700

    auto push!!!!!!!

--- scripts_for_rman/slenderman.sh
+++ scripts_for_rman/slenderman.sh
@@ -26,11 +26,17 @@ spinner $! "Clearing .git folder:"
 wait
 printf "\n=============================================\n\n"
 
+rm -r $4/$fname
+wait
 printf "Rendering:\n\n"
 
 /Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render -r rman -ris -rd "$4/$fname" -cam $6 -res 1920 1080 -s $7 -e $8 -im $fname -of 'OpenEXR' -fnc 'name_#.ext' -pad 3 -spool 'immediate rib, remote render' -proj "$1/$2" $1/$2/$3/$5
-logout
+wait
 
+cd $4/$fname
+while [ $(ls -l | grep -v ^l | wc -l) != $(expr $8 - $7) ]; do
+    echo "."
+done
 
 #spinner $! "Working on rendering..."
 ls -l | grep -v ^l | wc -l
